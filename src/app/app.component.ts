265780d592f21b2fd02bcf0b41e455ce62cde6a3
Brahian Stiven Garcia Torres <brahian.garcia@arus.com.co>
2020-12-08 20:24:40 -0500

    "initial commit"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ES_CONFIG } from '../config/esConfig';
+import { EN_CONFIG } from '../config/enConfig';
 
+export interface IMove {
+  moveName: string;
+  minDamage: number;
+  maxDamage: number;
+  attack: number;
+  blood: number;
+  firstSkill: string;
+  secondSkill: string;
+  totalSkill: number;
+  requiredPosition: string;
+  obtainedPosition: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'TWGMoveTable';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = ['moveName', 'minDamage', 'maxDamage', 'attack', 'blood', 'firstSkill', 'secondSkill', 'totalSkill', 'requiredPosition', 'obtainedPosition'];
+  dataSource: MatTableDataSource<IMove>;
+
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  language: string;
+
+  config = EN_CONFIG;
+
+  constructor() {
+    this.dataSource = new MatTableDataSource(this.config.moves);
+  }
+
+  setLanguage(language: string): void {
+    if (language === 'es') {
+      this.config = ES_CONFIG;
+    } else if (language === 'en') {
+      this.config = EN_CONFIG;
+    }
+  }
+
+  ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatSortModule } from '@angular/material/sort';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatCardModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
